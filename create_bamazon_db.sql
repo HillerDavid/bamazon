8a61ef5ac73946a7a043e172f8ccf20ea4a168e7
David Hiller <HillerDavid@users.noreply.github.com>
2018-11-09 13:55:47 -0500

    Added bamazonManager.js and bamazonSupervisor.js files.

--- create_bamazon_db.sql
+++ create_bamazon_db.sql
@@ -4,7 +4,7 @@ USE bamazon;
 
 CREATE TABLE IF NOT EXISTS products (
 	item_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-	product_name VARCHAR(30) NOT NULL,
+	product_name VARCHAR(50) NOT NULL,
     department_name VARCHAR(30) NOT NULL,
     price FLOAT(20, 2) NOT NULL,
     stock_quantity INT NOT NULL
